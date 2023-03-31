523501495edd80603b94c0bd922dc0fc9f146843
samiulalimsaad <samiulalimsaad@gmail.com>
2023-04-01 03:49:02 +0600

    setup files added

---
+++ setup/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+    printf("Hello you entered %d", a);
+    printf("Hello you entered %d", b);
+    return 0;
+}
