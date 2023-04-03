ef8f78c64a6dcbbcdc5eddf2fe8cdea047bb7318
samiulalimsaad <samiulalimsaad@gmail.com>
2023-04-04 02:12:01 +0600

    problem 3 solved

---
+++ Module 2.5: Practice Day 01 /problem-3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    scanf("%d", &num);
+
+    if (num % 2) {
+        printf("odd");
+    }
+    else {
+        printf("even");
+    }
+
+
+
+
+    return 0;
+}
