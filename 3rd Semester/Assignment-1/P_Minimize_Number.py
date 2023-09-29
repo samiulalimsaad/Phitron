6f22c94ef4833a6d06b0d96160042b1ea7593b3d
samiulalimsaad <samiulalimsaad@gmail.com>
2023-09-29 22:04:58 +0600

    replaced all

--- 3rd Semester/Assignment-1/P_Minimize_Number.py
+++ 3rd Semester/Assignment-1/P_Minimize_Number.py
@@ -1,9 +1,17 @@
 n = int(input())
 nums = list(map(int, input().split()))
 
+
+def is_valid(itr):
+    for i in itr:
+        if i % 2 != 0:
+            return False
+    return True
+
+
 c = 0
 
-while all(num % 2 == 0 for num in nums):
+while is_valid(nums):
     nums = [num // 2 for num in nums]
     c += 1
 
